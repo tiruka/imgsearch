68bc6c7dff0a4471af35f0cc07e46677b65ef34f
tnagai <nagai@zetacx.com>
2019-09-19 15:24:25 +0900

    file path modified

--- upload.py
+++ upload.py
@@ -1,12 +1,9 @@
 import os
+import glob
+
+from PIL import Image
 from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
-
-from keras.models import Sequential, load_model
-import keras
-import sys
-import numpy as np
-from PIL import Image
 
 from preprocess.predict import PredictImage
 from preprocess.gen_data import LoadImage
@@ -23,6 +20,11 @@ app.config['SECRET_KEY'] = os.urandom(24)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
+def ret_predicted_img_url(image_name):
+    fp = os.path.join(ORIGINAL_IMAGE_FOLDER, image_name, '*.jpg')
+    files = glob.glob(fp)
+    return sorted(files)[0]
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -38,29 +40,13 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             image_name, percentage = PredictImage(Image.open(filepath)).print_result()
-            print(image_name, percentage)
-            predicted_img_url = os.path.join(ORIGINAL_IMAGE_FOLDER, image_name)
+            predicted_img_url = ret_predicted_img_url(image_name)
             return render_template('result.html',
                                     original_img_url=filepath,
                                     predicted_img_url=predicted_img_url,
                                     percentage=percentage,)
-           
-
-            # image = Image.open(filepath)
-            # image = image.convert('RGB')
-            # image = image.resize((image_size, image_size))
-            # data = np.asarray(image)
-            # X = []
-            # X.append(data)
-            # X = np.array(X)
-
-            # result = model.predict([X])[0]
-            # predicted = result.argmax()
-            # percentage = int(result[predicted] * 100)
-            # return "Label:  " + classes[predicted] + ", Probability"+ str(percentage) + " %"
 
     return render_template('index.html')
-            # return redirect(url_for('uploaded_file', filename=filename))
 
 @app.route('/result')
 def result(filepath):
