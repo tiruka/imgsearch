8588773bedd0b272b63366233aaf5804f3783c27
tnagai <nagai@zetacx.com>
2019-09-20 15:37:08 +0900

    fixed typo

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 DATA_DIRECTORY = './static/data/img/'
 MODEL_PATH = './img_keras_cnn.h5'
-MAPPING_JSON = './index_label_mapping'
+MAPPING_JSON = './index_label_mapping.json'
 NP_DATA = './np_data.npy'
 UPLOAD_FOLDER = './static/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
